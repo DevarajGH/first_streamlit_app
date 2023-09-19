9e858a766aa976d1ac0160323aa917ac80a4c36c
DevarajGH <145459302+DevarajGH@users.noreply.github.com>
2023-09-19 19:12:37 +0530

    streamlit_app.py
    error check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit 
-streamlit.title('My Moms New Healthy Dinner') 
+streamlit.title("My Mom's New Healthy Diner") 
 streamlit.header('🥣 Breakfast Favourites') 
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
@@ -10,7 +10,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)  
