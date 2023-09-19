58cbc7d77e4335d169583717a647467997de51d4
DevarajGH <145459302+DevarajGH@users.noreply.github.com>
2023-09-19 18:36:15 +0530

    streamlit_app.py
    added footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('🥣 Breakfast Favourites')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑🍞Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
