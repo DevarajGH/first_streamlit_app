252b090071b376bb72f86068002ea6144158c328
DevarajGH <145459302+DevarajGH@users.noreply.github.com>
2023-09-19 19:08:01 +0530

    streamlit_app.py
    added strawberry

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.header('🥣 Breakfast Favourites')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑🍞Avacado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')  
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
