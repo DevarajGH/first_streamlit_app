dccfac6388264a48801a3258d29af640bedc7619
DevarajGH <145459302+DevarajGH@users.noreply.github.com>
2023-09-19 18:34:07 +0530

    streamlit_app.py
    latest menu with pics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit 
 streamlit.title('My Mom's New Healthy Dinner')
 
-streamlit.header('🥣 Breakfast Favourites')
+streamlit.header('🥣 Breakfast Favourites') 
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑🍞Avacado Toast')
