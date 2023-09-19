9e530d22141e9b225e566eb4f79a316fca6dd2d6
DevarajGH <145459302+DevarajGH@users.noreply.github.com>
2023-09-19 19:01:49 +0530

    streamlit_app.py
    added title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+streamlit.title('My Mom's New Healthy Dinner')
 streamlit.header('🥣 Breakfast Favourites') 
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🐔Kale, Spinach & Rocket Smoothie')
